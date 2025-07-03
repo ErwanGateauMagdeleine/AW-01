03fa8194ffb46c179e0d69b42f5c6e44d305d53c
Erwan <erwan.gateau-magdeleine@analog.com>
2025-07-03 12:41:23 +0200

    Paint textured background

--- plugin/src/PluginEditor.cpp
+++ plugin/src/PluginEditor.cpp
@@ -7,6 +7,10 @@ AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAud
       dummyKnob(*processorRef.parameters.getParameter("Dummy"), "dummy knob"),
       dummySliderAttachment(processorRef.parameters, "Dummy", dummyKnob)
 {
+    /* Load Background image */
+    textureImage = juce::ImageCache::getFromMemory(BinaryData::Metal009_1KPNG_Color_png,
+                                                   BinaryData::Metal009_1KPNG_Color_pngSize);
+
     addAndMakeVisible(dummyKnob);
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
@@ -20,8 +24,17 @@ AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
 //==============================================================================
 void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    /* Tile the background  texture */
+    if (textureImage.isValid())
+    {
+        for (int y = 0; y < getHeight(); y += textureImage.getHeight())
+        {
+            for (int x = 0; x < getWidth(); x += textureImage.getWidth())
+            {
+                g.drawImageAt(textureImage, x, y);
+            }
+        }
+    }
 }
 
 void AudioPluginAudioProcessorEditor::resized()
