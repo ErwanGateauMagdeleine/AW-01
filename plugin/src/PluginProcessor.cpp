c74ce22729d945255a91e4c99563e09eac3e1a6d
Erwan GM <erwan.magdeleine@gmail.com>
2026-03-03 19:49:13 +0100

    update repo structure

--- plugin/src/PluginProcessor.cpp
+++ plugin/src/PluginProcessor.cpp
@@ -175,17 +175,20 @@ juce::AudioProcessorEditor* AudioPluginAudioProcessor::createEditor()
 //==============================================================================
 void AudioPluginAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
-    juce::ignoreUnused (destData);
+    copyXmlToBinary (*parameters.copyState().createXml(), destData);
 }
 
 void AudioPluginAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
-    juce::ignoreUnused (data, sizeInBytes);
+    std::unique_ptr<juce::XmlElement> xmlState (getXmlFromBinary (data, sizeInBytes));
+
+    if (xmlState != nullptr)
+    {
+        if (xmlState->hasTagName(parameters.state.getType()))
+        {
+            parameters.replaceState(juce::ValueTree::fromXml (*xmlState));
+        }
+    }
 }
 
 //==============================================================================
